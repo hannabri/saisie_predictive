80a4c7e777b20377f31ac37d11385c4812692254
Clara <c.rosinafernandez@gmail.com>
2023-04-20 10:09:12 +0200

    cration des cornichons

--- tokenizer_cleaner.py
+++ tokenizer_cleaner.py
@@ -3,38 +3,34 @@ import re
 from nltk.tokenize import word_tokenize
 import pickle
 
+emojis = [":-)", "<3", ":]", ";)", ";-)", ":D", ":-D", ";P", ";-P", ":P", ":-P", "8)", "8-)", ":|", ":-|", ":(", ":-(", "o_O", "o.O", ":/", ":-/", ":O", ":-O", "O_O", "O.O", "o_o"]
+
+def clean_and_tokenize(text):
+    for emoji in emojis:
+        text = text.replace(emoji, "")
+    text = re.sub(r'[^\w\s]', '', text.lower())  # Supprimer les caractères non alphanumériques et mettre en minuscules
+    tokens = word_tokenize(text)  # Tokeniser le texte
+    return tokens
+
 def tokenize():
     # Charger le fichier Excel en DataFrame
     sms_df = pd.read_excel('train_sms.xlsx')
 
-    # Convertir la colonne qui contient les sms en chaîne de caractères (je sais pas pq sinon ça ne marche pas, alors que normalemnet c'est tout des strings)
+    # Convertir la colonne qui contient les sms en chaîne de caractères
     sms_df['SMS_ANON'] = sms_df['SMS_ANON'].astype(str)
 
-    emojis = [":-)", "<3", ":]", ";)", ";-)", ":D", ":-D", ";P", ";-P", ":P", ":-P", "8)", "8-)", ":|", ":-|", ":(", ":-(", "o_O", "o.O", ":/", ":-/", ":O", ":-O", "O_O", "O.O", "o_o"]
-
     # Nettoyer et tokeniser le texte
-
     sms_df['tokens'] = sms_df['SMS_ANON'].apply(clean_and_tokenize)
 
-    def clean_and_tokenize(text):
-        for emoji in emojis:text = text.replace(emoji, "")
-        text = re.sub(r'[^\w\s]', '', text.lower())  # Supprimer les caractères non alphanumériques et mettre en minuscules
-        tokens = word_tokenize(text)  # Tokeniser le texte
-        return tokens
-
-#Créer un fichier pickel
-    tokens = clean_and_tokenize(sms_df['SMS_ANON'].astype(str))
-
-    with open("tokens.pkl", "wb") as file:
-        pickle.dump(tokens, file)
-
-    vocab = []
+    list_tokens = []
     for l in sms_df["tokens"].tolist(): 
-        vocab.extend(l)
+        list_tokens.extend(l)
 
-    return vocab
+    # sms_df.to_excel('train_sms_transformed.xlsx', index=False)
+    
+    return list_tokens
 
-    #Cŕeer un nouveau fichier excel
 
-    #sms_df.to_excel('train_sms_transformed.xlsx', index=False)
-
+    # Créer un fichier pickle
+with open("tokens.pkl", "wb") as file:
+    pickle.dump(tokenize(), file)
