d62b480f22d6310bd0217334f836b9ca4107e78c
Anna <barnay.anna@gmail.com>
2023-06-03 12:00:24 +0200

    predtrigrammesAnna

--- project.py
+++ project.py
@@ -66,35 +66,25 @@ def fullDict (d, listTokens): #listTokens = liste de liste
                     d[listTokens[i][j-1]]=Contexte(listTokens[i][j-1]) # créer le contexte si il existe pas (contexte = listTokens[i][j-1])
                 d[listTokens[i][j-1]].add_word(listTokens[i][j]) #ajouter une occurence au mot qui suit le contexte
 
-def fullDictTriGrammes (d, listTokens): #listTokens = liste de liste
-    for i in range(len(listTokens)): # pour tous les sms
-        for j in range(2,len(listTokens[i]),1): # du 2eme au dernier token
-                if (listTokens[i][j-1]) not in d:
-                    d[listTokens[i][j-1]]=Contexte(listTokens[i][j-1]) # créer le contexte si il existe pas (contexte = listTokens[i][j-1])
-                d[listTokens[i][j-1]].add_word(listTokens[i][j]) #ajouter une occurence au mot qui suit le contexte
-                if (listTokens[i][j-2]+" "+listTokens[i][j-1]) not in d:
-                    d[listTokens[i][j-2]+" "+listTokens[i][j-1]]=Contexte(listTokens[i][j-2]+" "+listTokens[i][j-1]) # créer le contexte si il existe pas (contexte = listTokens[i][j-1])
-                d[listTokens[i][j-2]+" "+listTokens[i][j-1]].add_word(listTokens[i][j]) #ajouter une occurence au mot qui suit le contexte
-
-def fullDictTest (d, listTokens, taille): #listTokens = liste de liste
-    for i in range(len(listTokens)): # pour tous les sms
-        for j in range(1,len(listTokens[i]),1): # du 2eme au dernier token
+def fullDictTest (d, listTokens, nbGramme):
+    for i in range(len(listTokens)):
+        for j in range(1,len(listTokens[i]),1):
                 contexte=[]
-                for z in range (1, min(taille,j),1):
+                for z in range (1, min(nbGramme,j),1):
                     contexte.insert(0,listTokens[i][j-z])
-                if (contexte) not in d:
-                    d[contexte]=Contexte(contexte) # créer le contexte si il existe pas (contexte = listTokens[i][j-1])
-                d[contexte].add_word(listTokens[i][j]) #ajouter une occurence au mot qui suit le contexte
+                    if tuple(contexte) not in d:
+                        d[tuple(contexte)]=Contexte(tuple(contexte))
+                    d[tuple(contexte)].add_word(listTokens[i][j])
 
 def updateWordsPred (d):
     for v in d.values() :
         v.update_wordsPred()
 
-def initDictio ():
+def initDictio (nbGramme):
     with open("tokens.pkl", "rb") as file:
             listTokens = pickle.load(file)
     dictio={}
-    fullDict(dictio, listTokens)
+    fullDictTest(dictio, listTokens,nbGramme)
     updateWordsPred(dictio)
     return dictio
 
@@ -103,22 +93,7 @@ def prediction ():
     saisie = " "
     while saisie!="":
         saisie = input("mot du contexte : ")
-        print(dictio[saisie].wordsPred if saisie in dictio else [])
-
-def initDictio2 ():
-    with open("tokens.pkl", "rb") as file:
-            listTokens = pickle.load(file)
-    dictio={}
-    fullDictTriGrammes(dictio, listTokens)
-    updateWordsPred(dictio)
-    return dictio
-
-def prediction2 ():
-    dictio = initDictio2()
-    saisie = " "
-    while saisie!="":
-        saisie = input("mot du contexte : ")
-        print(dictio[saisie].wordsPred if saisie in dictio else [])
+        print(dictio[tuple(saisie.split())].wordsPred if tuple(saisie.split()) in dictio else [])
 
 
 #partie test prediction
@@ -140,14 +115,14 @@ def testprediction2 (corpusTest,dictio,marge):
     total=0
     for j in range(len(corpusTest)):
         for i in range(1,len(corpusTest[j])-1):
-            motsPredTri = predictionWord(dictio, corpusTest[j][i-1]+" "+corpusTest[j][i], marge)
+            motsPredTri = predictionWord(dictio, (corpusTest[j][i-1],corpusTest[j][i]), marge)
             trouve=False
             if ( len(motsPredTri) > 0 ) :
                 if (corpusTest[j][i+1] in motsPredTri):
                     correct +=1
                     trouve=True
             
-            if (trouve==False) and (corpusTest[j][i+1] in predictionWord(dictio, corpusTest[j][i], marge)):
+            if (trouve==False) and (corpusTest[j][i+1] in predictionWord(dictio, (corpusTest[j][i],), marge)):
                 correct+=1
             total+=1
     return (correct/total)
@@ -158,9 +133,9 @@ def testprediction3 (corpusTest,dictio):
     total=0
     for j in range(len(corpusTest)):
         for i in range(1,len(corpusTest[j])-1):
-            motsPred = predictionWord(dictio, corpusTest[j][i-1]+" "+corpusTest[j][i],3)
+            motsPred = predictionWord(dictio, (corpusTest[j][i-1],corpusTest[j][i]),3)
             z=0
-            motsPredBi = predictionWord(dictio, corpusTest[j][i],3)
+            motsPredBi = predictionWord(dictio, (corpusTest[j][i],),3)
             while len(motsPred)<3 and (z<len(motsPredBi)):
                 if motsPredBi[z] not in motsPred :
                     motsPred.append(motsPredBi[z])
@@ -170,10 +145,9 @@ def testprediction3 (corpusTest,dictio):
             total+=1
     return (correct/total)
 
-dictio2 = initDictio2()
+dictio = initDictio(3)
 test = tokenize_test()
 #for i in range(1,11,1):
 #     print(str(i)+ " mots prédits : "+str(testprediction2(test, dictio, i)))
 #prediction()
-#prediction2()
-print(testprediction3(test, dictio2))
+print(testprediction3(test, dictio))
