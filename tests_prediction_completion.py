0affb715fad6af3e834fbec41846d8e3b8778dae
Hanna Brinkmann <hanna_brinkmann@outlook.com>
2023-06-11 19:56:43 +0200

    Ajout fonctions test

--- tests_prediction_completion.py
+++ tests_prediction_completion.py
@@ -1,3 +1,4 @@
+from serialization import *
 #partie test prediction
 
 def predictionWord(dictio, wordContexte, marge): # marge = nb de mots prédits
@@ -69,6 +70,44 @@ def testcompletionprediction (dictio, trie, corpusTest, nbWords, pdsPred, sizePr
                 correct+=1
             total+=1
     return (correct/total)
+
+
+#Prédiction 
+
+#dictio=initDictio(2)
+#test = tokenize_test()
+#print(rtUnkWords(test, dictio))
+
+
+dictio = initDictio(3)
+test = load("test")
+max =0
+for i in range (1,11,1):
+    for j in range (0,i+1,1):
+        result = testPred(test, dictio, j, i-j)
+        print("i : "+str(i)+ " -> nbB : "+str(j)+" ; "+"nbT : "+str(i-j)+" = "+str(result))
+        if result>max:
+            max=result
+#print(max)
+#for i in range(1,11,1):
+#     print(str(i)+ " mots prédits : "+str(testprediction2(test, dictio, i)))
+#prediction()
+#print(testprediction3(test, dictio))
+
+# Complétion
+
+# dictio = initDictio(2)
+# for i in range(1,11,1):
+#     for j in range(1,6,1):
+#         print(f'nb mots prédits : {i} , taille du prefixe : {j} , score = {testcompletionprediction2(dictio, trie,test, i, 5, j)}')
+
+# Charger les tokens
+tokens = load("corpus")
+tokens_test = load("test")
+
+for i in range(1,11,1):
+    for j in range(1,6,1):
+        print(f'nb mots prédits : {i} , taille du prefixe : {j} , score = {testcompletion(trie,tokens_test, j, i)}')
 
 
 
